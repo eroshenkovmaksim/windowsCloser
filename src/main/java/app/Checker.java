1d98e5723d3396875a3b779ca9834aec8ac20f4e
maksim eroshenkov <maksim.eroshenkov@netcracker.com>
2019-08-04 18:00:27 +0300

    init

---
+++ src/main/java/app/Checker.java
@@ -0,0 +1,13 @@
+package main.java.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Checker {
+  public static void main(String[] args) {
+    System.out.println("");
+  }
+}
